d13467b98f96169e5cd24846a4119ddb4152b227
oxnz <yunxinyi@gmail.com>
2015-03-04 17:50:05 +0800

    add dummy inspect

---
+++ tool/inspect.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+# 
+#================================================================
+#
+# Filename:	inspect.sh
+#
+# Author:		Oxnz
+# Email:		yunxinyi@gmail.com
+# Created:		[2015-03-04 17:49:37 CST]
+# Last-update:	2015-03-04 17:49:37 CST
+# Description: ANCHOR
+#
+# Version:		0.0.1
+# Revision:	[None]
+# Revision history:	[None]
+# Date Author Remarks:	[None]
+#
+# License:
+# Copyright (c) 2013 Oxnz
+#
+# Distributed under terms of the [LICENSE] license.
+# [license]
+#
+#====================================== [vim template v1.0] =====
+#
+
+# check what's wrong

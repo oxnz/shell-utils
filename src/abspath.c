875e80988648f6b9c4787f1d5c73aec943985880
oxnz <yunxinyi@gmail.com>
2015-11-27 10:58:35 +0800

    update abspath & Makefile; rename plug->plugin

--- src/abspath.c
+++ src/abspath.c
@@ -12,7 +12,7 @@
  * Revision history:	[NONE]
  * Date Author Remarks:	[NONE]
  *
- * License: 
+ * License:
  * Copyright (c) 2013 Oxnz
  *
  * Distributed under terms of the [LICENSE] license.
@@ -27,16 +27,24 @@
 
 int main(int argc, char *argv[]) {
 	char resolved_path[PATH_MAX];
-	if (argc != 2) {
-		fprintf(stderr, "abspath: missing oprand\nTry 'abspath --help' for more information.\n");
+	int status = 0;
+	int verbose = 0;
+
+	if (argc < 2) {
+		fprintf(stderr, "%s: missing oprand\n"
+				"Try 'abspath --help' for more information.\n", *argv);
 		return 1;
 	}
-	char* ret = realpath(argv[1], resolved_path);
-	if (NULL == ret) {
-		perror("realpath");
-		return 1;
-	} else {
-		printf("%s\n", resolved_path);
+	while (*++argv) {
+		char* ret = realpath(*argv, resolved_path);
+		if (NULL == ret) {
+			if (verbose) {
+				perror("realpath");
+			}
+			status = 1;
+		} else {
+			printf("%s\n", resolved_path);
+		}
 	}
-	return 0;
+	return status;
 }
